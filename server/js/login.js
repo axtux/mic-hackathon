c515ba526601290a1cc8836a30403b140009fada
Guillaume Huysmans <ghuysmans99@gmail.com>
2015-04-19 11:29:49 +0200

    Removed the debug login alert

--- server/js/login.js
+++ server/js/login.js
@@ -3,7 +3,7 @@ function $i(id) {
 }
 function dochap(ch) {
 	var hp = sha1($i('p').value);
-	alert((hp+$i('e').value.toLowerCase()+ch));
+	//alert((hp+$i('e').value.toLowerCase()+ch));
 	$i('p').value = sha1(hp+$i('e').value.toLowerCase()+ch);
 	return true;
 }
