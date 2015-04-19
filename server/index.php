33a59ee63c0f5998766885d92f78f03aabf46c2d
Guillaume Huysmans <ghuysmans99@gmail.com>
2015-04-19 11:47:43 +0200

    Ported changes from home to index, removed the latter

--- server/index.php
+++ server/index.php
@@ -90,8 +90,6 @@
               <div class = "form-group" id="profile">
                 <label for="message-text" class="control-label">E-mail :</label>
                 <input type="text" class="form-control" name="email">
-                <label for="message-text" class="control-label">password :</label>
-                <input type="password" class="form-control" name="password">
               </div>
               <div class = "form-group" id="link">
                 <label for="message-text" class="control-label">Link :</label>
@@ -103,12 +101,15 @@
               </div>
               <div id="gps">
                 <div class = "form-group">
+                  <button type="button" class="btn btn-info" onclick="getLocation()">Locate your device</button>
+                </div>
+                <div class = "form-group">
                   <label for="message-text" class="control-label">Latitude :</label>
-                  <input type="text" class="form-control" name="latitude">
+                  <input type="text" class="form-control" name="latitude" id="lat">
                 </div>
                 <div class="form-group">
                   <label for="message-text" class="control-label">Longitude :</label>
-                  <input type="text" class="form-control" name="longitude">
+                  <input type="text" class="form-control" name="longitude" id="lon">
                 </div>
               </div>
               <div id="edge">
@@ -143,10 +144,11 @@
       &copy; Graphisy 2015
     </div>
     
-    <script src="./js/jquery-2.1.3.min.js"></script>
-    <script src="./js/bootstrap.min.js"></script>
-    <script src="./js/sigma.min.js"></script>
-    <script src="./js/functions.js"></script>
+    <script src="js/jquery-2.1.3.min.js"></script>
+    <script src="js/gps.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/sigma.min.js"></script>
+    <script src="js/functions.js"></script>
     <script>
       var nodes;// = [{"alpha":null,"beta":null,"relation":null,"id_node":"43","name":"EDO","description":"EDO","created_on":"2015-04-18 07:55:34","is_deleted":"1","owner":"42","email":null,"link":null,"path":null,"is_previewable":null,"latitude":null,"longitude":null},{"alpha":"47","beta":"43","relation":"has","id_node":"47","name":"Analyse B","description":"Cour de math","created_on":"2015-04-18 08:18:35","is_deleted":"0","owner":"42","email":null,"link":null,"path":null,"is_previewable":null,"latitude":null,"longitude":null},{"alpha":"50","beta":"43","relation":"applies","id_node":"50","name":"(x)''= ma","description":"<h1>Power of physics<\/h1>","created_on":"2015-04-18 09:10:13","is_deleted":"0","owner":"42","email":null,"link":null,"path":null,"is_previewable":null,"latitude":null,"longitude":null},{"alpha":"43","beta":"44","relation":"has","id_node":"44","name":"Exo-edo","description":"Methode pour calculer les edo","created_on":"2015-04-18 07:57:13","is_deleted":"0","owner":"42","email":null,"link":null,"path":"pouet.png","is_previewable":"1","latitude":null,"longitude":null},{"alpha":"43","beta":"45","relation":"requires","id_node":"45","name":"Polynome","description":"Polynome","created_on":"0000-00-00 00:00:00","is_deleted":"0","owner":"42","email":null,"link":null,"path":null,"is_previewable":null,"latitude":null,"longitude":null},{"alpha":"43","beta":"46","relation":"link","id_node":"46","name":"EDO-wiki","description":"Wiki-EDO","created_on":"2015-04-18 08:12:03","is_deleted":"0","owner":"42","email":null,"link":"http:\/\/fr.wikipedia.org\/wiki\/%C3%89quation_di","path":null,"is_previewable":null,"latitude":null,"longitude":null}];
       var current_node = '43';
